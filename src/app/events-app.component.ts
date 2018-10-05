31f4502f46c390ec39538390d8754ad97eb21578
Nic <ngrunks@gmail.com>
2018-10-04 21:30:27 -0500

    Initial Commit

--- src/app/events-app.component.ts
+++ src/app/events-app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'app-events',
+  template: `
+    <app-navbar></app-navbar>
+    <app-events-list></app-events-list>
+    `
 })
-export class AppComponent {
+export class EventsAppComponent {
   title = 'app';
 }
